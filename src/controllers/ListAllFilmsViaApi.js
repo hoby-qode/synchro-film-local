18905cffee1efd840f3625744d40ede7724cc4b4
marcel <marcel@medialibs.com>
2024-04-09 18:41:37 +0300

    feat: récupération de toutes les films de l'api

--- src/controllers/ListAllFilmsViaApi.js
+++ src/controllers/ListAllFilmsViaApi.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
+const fs = require('fs')
 const axios = require('axios')
 
 const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzQzYmRhMTViZjgwNjU2MTEyZjQzMWVkYjFiY2M3NiIsInN1YiI6IjY1YTRmM2Q3OGEwZTliMDEyZWI0NjE3NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZJc_GUl1LWfmJovPq51s3MiFuwsKAaQeGH6YXQSRjUI'; // Remplacez cela par votre clé d'API TMDB
 
 async function fetchMovies(page) {
-    const url = `https://api.themoviedb.org/3/discover/movie&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`;
+    const url = `https://api.themoviedb.org/3/discover/movie?language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`;
     const options = {
         headers: {
         accept: 'application/json',
@@ -12,43 +13,49 @@ async function fetchMovies(page) {
         },
     }
     const response = await axios.get(url, options)
-    const data = await response.json();
-    return data.results;
+    // const data = await response.json();
+    console.log(response)
+    return response.data;
 }
 
 async function getAllMovies() {
-    // try {
-    //     let allMovies = [];
-    //     let page = 1;
-    //     let totalPages = 1;
+    try {
+        let allMovies = [];
+        let page = 1;
+        let totalPages = 1;
 
-    //     while (page <= totalPages) {
-    //         const movies = await fetchMovies(page);
-    //         console.log(movies)
-    //         // allMovies = allMovies.concat(movies.filter(movie => movie.media_type === 'movie'));
-    //         // totalPages = Math.min(movies.total_pages, 1000); // Limiter le nombre total de pages à 1000
-    //         page++;
-    //     }
+        do {
+            const movies = await fetchMovies(page);
+            allMovies = allMovies.concat(movies.results);
+            totalPages = movies.total_pages; // Obtenez le nombre total de pages de la première réponse
+            page++;
+        } while (page <= totalPages && page <= 200); // Limiter le nombre total de pages à 1000
 
-    //     return allMovies;
-    // } catch (error) {
-    //     console.error('Erreur lors de la récupération des films :', error);
-    //     throw error;
-    // }
-    const movies = await fetchMovies(1);
-           return movies
+        return allMovies;
+    } catch (error) {
+        console.error('Erreur lors de la récupération des films :', error);
+        throw error;
+    }
 }
 
 async function main() {
     try {
         const movies = await getAllMovies();
-        console.log('Liste des films :');
-        movies.forEach(movie => {
-            console.log(movie.title);
-        });
+        const titles = movies.map(movie => movie.title).join('\n'); // Crée une chaîne avec les titres des films séparés par un saut de ligne
+        await writeFileAsync('movies.txt', titles);
     } catch (error) {
         console.error('Une erreur est survenue :', error);
     }
 }
-
+async function writeFileAsync(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
 main();
